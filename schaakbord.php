288393552df4ac1cb05c6cf10494da441ca958cd
daan <2087884@talnet.nl>
2020-03-02 13:25:36 +0100

    Uitwerking van de exercise

--- schaakbord.php
+++ schaakbord.php
@@ -10,18 +10,18 @@
 <table border="1px">
 
 <?php
-    for ($row=1; $row <= 8; $row++) {
-        echo "<tr>";
-            for($col=1; $col <= 8; $col++) {
-                $total = $row + $col;
-                if ($total%2 == 0) {
-                    echo "<td height=40px width=40px bgcolor=#FFFFFF></td>";
-                } else {
-                    echo "<td height=40px width=40px bgcolor=#000></td>";
-                }
+for ($row=1; $row <= 8; $row++) {
+    echo "<tr>";
+        for($col=1; $col <= 8; $col++) {
+            $total = $row + $col;
+            if ($total%2 == 0) {
+                echo "<td height=40px width=40px bgcolor=#FFFFFF></td>";
+            } else {
+                echo "<td height=40px width=40px bgcolor=#000></td>";
             }
-        echo "</tr>";
-    }
+        }
+    echo "</tr>";
+}
 ?>
 </table>
     
